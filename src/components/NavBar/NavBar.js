3c083fa2690c5d3a43ab0504b4a46a06c7fe57fd
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-05-20 20:35:30 -0600

    navbar styling

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./NavBar.css"
 
 
 
 function Navbar() {
   return (
     <div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-info">
-    <a class="navbar-brand" href="#aboutme">Lauren Penenburgh</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+    <nav className="navbar navbar-expand-lg navbar-dark bg-info">
+    <a className="navbar-brand" href="#aboutme">Lauren Penenburgh</a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
       aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
       <li className="nav-item">
             <Link
               to="/"
