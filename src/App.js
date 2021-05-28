687599d940d361e06cb865fd39ba9fbb697f91f8
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-05-27 19:01:04 -0600

    reworked router-added process.env

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Contact from "./pages/Contact";
 import Portfolio from "./pages/Portfolio";
 //import NavBar from "./components/NavBar/NavBar";
@@ -10,17 +10,19 @@ import About from "./pages/About";
 
 function App() {
   return (
-    <Router>
-    <div>
-      <Wrapper>
-        <Route exact path="/" component={About} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/contact" component={Contact} />
-      </Wrapper>
-    </div>
-  </Router>
-    
+    <Router basename={process.env.PUBLIC_URL}>
+      <div>
+        <Wrapper>
+          <Switch>
+            <Route exact path="/" component={About} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/portfolio" component={Portfolio} />
+            <Route exact path="/contact" component={Contact} />
+          </Switch>
+        </Wrapper>
+      </div>
+    </Router>
+
   );
 }
 
