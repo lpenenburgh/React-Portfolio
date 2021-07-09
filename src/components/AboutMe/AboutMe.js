19128df7ed5cc74497d8bd23298af9b921167812
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-07-09 16:32:12 -0600

    technical skills added

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,6 +1,8 @@
 import React from "react";
 import headshot from "../../images/headshot3.png";
 import aboutme from "../../images/aboutme5.png";
+import { SiJavascript, SiHtml5 , SiCss3 , SiMysql, SiMongodb , SiReact , SiPostman , SiJquery } from 'react-icons/si';
+import {FaNode } from 'react-icons/fa';
 import "./AboutMe.css";
 
 function AboutMe() {
@@ -14,8 +16,10 @@ function AboutMe() {
                <div className="card text-white bg-info mb-3">
                  <div className="card-body">
                    <p className="card-text">Welcome! My name is Lauren Penenburgh. I am a full stack web developer living in Steamboat Springs, Colorado. I recently earned a certificate in full stack web development from the University of Denver Coding Bootcamp to leverage my background in customer service to create utile
-              applications to solve everyday problems. In my free time, I enjoy learning to code, skiing, hiking, and all types of crafting.
+              applications to solve everyday problems. In my free time, I enjoy learning to code, skiing, hiking, and all types of crafting. 
                      </p>
+                    <h4 className="card-text-skills">Technical Skills</h4> 
+                    <p className="skillIcon"><SiHtml5 /> <SiCss3 /> <SiJavascript /> <FaNode /> <SiMysql /> <SiMongodb /> <SiReact /> <SiPostman /> <SiJquery /></p>
                  </div>
                </div>
              </div>
--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -23,8 +23,8 @@ function ContactMe() {
                   <h5 className="card-title" id="contacttitle">Phone:</h5>
                   <p className="card-text" id="contacttext">(703)969-3123</p>
                   <a href="https://drive.google.com/file/d/1ccXvlsYT6WXpUpkgrLoCtAQyAz-jN9KS/view?usp=sharing" target="_blank" rel=" noopener noreferrer"><h5 className="card-title contactLinks" >Resume {pdf}</h5></a>
-                  <a href="https://github.com/lpenenburgh"  target="_blank" rel="noopener noreferrer"><h5 className="card-title contactLinks">{github}</h5></a>
-                  <a href="https://www.linkedin.com/in/lauren-penenburgh-00852a99/" target="_blank" rel=" noopener noreferrer"><h5 className="card-title contactLinks">{linkedin}</h5></a>
+                  <a href="https://github.com/lpenenburgh"  target="_blank" rel="noopener noreferrer"><h5 className="card-title contactLinks">Github {github}</h5></a>
+                  <a href="https://www.linkedin.com/in/lauren-penenburgh-00852a99/" target="_blank" rel=" noopener noreferrer"><h5 className="card-title contactLinks">LinkedIn {linkedin}</h5></a>
                 </div>
               </div>
             </div>
