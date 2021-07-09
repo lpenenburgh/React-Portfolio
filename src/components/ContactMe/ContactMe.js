05f79b696a78065c48e41d77c52af0da9f38b9e0
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-07-09 14:52:45 -0600

    changed project images

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -1,6 +1,10 @@
 import React from "react";
 import contactme from "../../images/contactme.png";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { fagithub } from '@fortawesome/free-solid-svg-icons';
 import "./ContactMe.css";
+
+// const element = <FontAwesomeIcon icon={fagithub } />
 
 function ContactMe() {
     return(
@@ -15,7 +19,7 @@ function ContactMe() {
                   <p className="card-text" id="contacttext">lpenenburgh@gmail.com</p>
                   <h5 className="card-title" id="contacttitle">Phone:</h5>
                   <p className="card-text" id="contacttext">(703)969-3123</p>
-                  <a href="https://github.com/lpenenburgh"  target="_blank" rel="noopener noreferrer"><h5 className="card-title contactLinks">GitHub</h5></a>
+                  <a href="https://github.com/lpenenburgh"  target="_blank" rel="noopener noreferrer"><h5 className="card-title contactLinks">Github</h5></a>
                   <a href="https://www.linkedin.com/in/lauren-penenburgh-00852a99/" target="_blank" rel=" noopener noreferrer"><h5 className="card-title contactLinks">LinkedIn</h5></a>
                   <a href="https://drive.google.com/file/d/1ccXvlsYT6WXpUpkgrLoCtAQyAz-jN9KS/view?usp=sharing" target="_blank" rel=" noopener noreferrer"><h5 className="card-title contactLinks" >Resume</h5></a>
                 </div>
